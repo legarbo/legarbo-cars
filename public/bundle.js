fe46ca7ad0caea1e081a9ffe778600124d4f8dc0
Leopoldo Garcia <legarbo@gmail.com>
2016-09-09 22:08:26 -0400

    switch to hashHistory

--- public/bundle.js
+++ public/bundle.js
@@ -80,7 +80,7 @@
 	  null,
 	  _react2.default.createElement(
 	    _reactRouter.Route,
-	    { path: '/', component: _mainComponent2.default, history: _reactRouter.browserHistory },
+	    { path: '/', component: _mainComponent2.default, history: _reactRouter.hashHistory },
 	    _react2.default.createElement(_reactRouter.IndexRoute, { component: _homeComponent2.default }),
 	    _react2.default.createElement(_reactRouter.Route, { path: '/cars', component: _carComponent2.default }),
 	    _react2.default.createElement(_reactRouter.Route, { path: '/about', component: _aboutComponent2.default })
@@ -27818,7 +27818,7 @@
   \***************************************/
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
@@ -27829,6 +27829,8 @@
 	var _react = __webpack_require__(/*! react */ 1);
 	
 	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactRouter = __webpack_require__(/*! react-router */ 172);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
@@ -27849,53 +27851,57 @@
 	  }
 	
 	  _createClass(Main, [{
-	    key: "render",
+	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
-	        "div",
+	        'div',
 	        null,
 	        _react2.default.createElement(
-	          "nav",
-	          { className: "navbar navbar-default" },
+	          'nav',
+	          { className: 'navbar navbar-default' },
 	          _react2.default.createElement(
-	            "div",
-	            { className: "container-fluid" },
+	            'div',
+	            { className: 'container-fluid' },
 	            _react2.default.createElement(
-	              "div",
-	              { className: "navbar-header" },
+	              'div',
+	              { className: 'navbar-header' },
 	              _react2.default.createElement(
-	                "a",
-	                { className: "navbar-brand", href: "#" },
-	                "Legarbo Cars"
+	                'a',
+	                { className: 'navbar-brand', href: '#' },
+	                'Legarbo Cars'
 	              )
 	            ),
 	            _react2.default.createElement(
-	              "div",
-	              { className: "collapse navbar-collapse", id: "bs-example-navbar-collapse-1" },
+	              'div',
+	              { className: 'collapse navbar-collapse', id: 'bs-example-navbar-collapse-1' },
 	              _react2.default.createElement(
-	                "ul",
-	                { className: "nav navbar-nav" },
+	                'ul',
+	                { className: 'nav navbar-nav' },
 	                _react2.default.createElement(
-	                  "li",
-	                  { className: "active" },
+	                  'li',
+	                  null,
 	                  _react2.default.createElement(
-	                    "a",
-	                    { href: "#" },
-	                    "Link ",
-	                    _react2.default.createElement(
-	                      "span",
-	                      { className: "sr-only" },
-	                      "(current)"
-	                    )
+	                    _reactRouter.Link,
+	                    { to: '/' },
+	                    'Home'
 	                  )
 	                ),
 	                _react2.default.createElement(
-	                  "li",
+	                  'li',
 	                  null,
 	                  _react2.default.createElement(
-	                    "a",
-	                    { href: "#" },
-	                    "Link"
+	                    _reactRouter.Link,
+	                    { to: '/cars' },
+	                    'Cars'
+	                  )
+	                ),
+	                _react2.default.createElement(
+	                  'li',
+	                  null,
+	                  _react2.default.createElement(
+	                    _reactRouter.Link,
+	                    { to: '/about' },
+	                    'About'
 	                  )
 	                )
 	              )
@@ -27903,8 +27909,8 @@
 	          )
 	        ),
 	        _react2.default.createElement(
-	          "div",
-	          { className: "container" },
+	          'div',
+	          { className: 'container' },
 	          this.props.children
 	        )
 	      );
@@ -28030,7 +28036,7 @@
 	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -28045,29 +28051,110 @@
 	
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } // ./src/car/car.component.jsx
+	
 	
 	var Car = function (_Component) {
-	  _inherits(Car, _Component);
+	    _inherits(Car, _Component);
 	
-	  function Car() {
-	    _classCallCheck(this, Car);
+	    // Constructor is responsible for setting up props and setting initial stte
+	    function Car(props) {
+	        _classCallCheck(this, Car);
 	
-	    return _possibleConstructorReturn(this, (Car.__proto__ || Object.getPrototypeOf(Car)).apply(this, arguments));
-	  }
+	        // Set initial state
+	        var _this = _possibleConstructorReturn(this, (Car.__proto__ || Object.getPrototypeOf(Car)).call(this, props));
+	        // Pass props to the parent component
 	
-	  _createClass(Car, [{
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement(
-	        'h1',
-	        null,
-	        'Cars page'
-	      );
+	
+	        _this.state = {
+	            // State needed
+	            cars: []
+	        };
+	        return _this;
 	    }
-	  }]);
 	
-	  return Car;
+	    _createClass(Car, [{
+	        key: 'componentDidMount',
+	        value: function componentDidMount() {
+	            // Static data
+	            var data = [{
+	                id: 1,
+	                name: 'Honda Accord Crosstour',
+	                year: '2010',
+	                model: 'Accord Crosstour',
+	                make: 'Honda',
+	                media: 'http://media.ed.edmunds-media.com/honda/accord-crosstour/2010/oem/2010_honda_accord-crosstour_4dr-hatchback_ex-l_fq_oem_4_500.jpg',
+	                price: '$16,811'
+	
+	            }, {
+	                id: 2,
+	                name: 'Mercedes-Benz AMG GT Coupe',
+	                year: '2016',
+	                model: 'AMG',
+	                make: 'Mercedes Benz',
+	                media: 'http://media.ed.edmunds-media.com/mercedes-benz/amg-gt/2016/oem/2016_mercedes-benz_amg-gt_coupe_s_fq_oem_1_717.jpg',
+	                price: '$138,157'
+	
+	            }, {
+	                id: 3,
+	                name: 'BMW X6 SUV',
+	                year: '2016',
+	                model: 'X6',
+	                make: 'BMW',
+	                media: 'http://media.ed.edmunds-media.com/bmw/x6/2016/oem/2016_bmw_x6_4dr-suv_xdrive50i_fq_oem_1_717.jpg',
+	                price: '$68,999'
+	            }, {
+	                id: 4,
+	                name: 'Ford Edge SUV',
+	                year: '2016',
+	                model: 'Edge',
+	                make: 'Ford',
+	                media: 'http://media.ed.edmunds-media.com/ford/edge/2016/oem/2016_ford_edge_4dr-suv_sport_fq_oem_6_717.jpg',
+	                price: '$36,275'
+	            }, {
+	                id: 5,
+	                name: 'Dodge Viper Coupe',
+	                year: '2017',
+	                model: 'Viper',
+	                make: 'Dodge',
+	                media: 'http://media.ed.edmunds-media.com/dodge/viper/2017/oem/2017_dodge_viper_coupe_acr_fq_oem_3_717.jpg',
+	                price: '$123,890'
+	            }];
+	            // Update state
+	            this.setState({ cars: data });
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            // Map through cars and return linked cars
+	            var carNode = this.state.cars.map(function (car) {
+	                return _react2.default.createElement(
+	                    'a',
+	                    {
+	                        href: '#',
+	                        className: 'list-group-item',
+	                        key: car.id },
+	                    car.name
+	                );
+	            });
+	            return _react2.default.createElement(
+	                'div',
+	                null,
+	                _react2.default.createElement(
+	                    'h1',
+	                    null,
+	                    'Cars page'
+	                ),
+	                _react2.default.createElement(
+	                    'div',
+	                    { className: 'list-group' },
+	                    carNode
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return Car;
 	}(_react.Component);
 	
 	exports.default = Car;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import Main from './common/main.component.jsx'
 import Home from './common/home.component.jsx'
@@ -10,7 +10,7 @@ import Car from './car/car.component.jsx'
 
 render (
   <Router>
-    <Route path="/" component={Main} history={browserHistory}>
+    <Route path="/" component={Main} history={hashHistory}>
       <IndexRoute component={Home}/>
       <Route path='/cars' component={Car}/>
       <Route path='/about' component={About}/>
