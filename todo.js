f38142b47ae3613dff29d855e42d4b0aaa7f58a4
Leopoldo Garcia <legarbo@gmail.com>
2016-09-13 21:13:41 -0400

    rename index.html -> 200.html

---
+++ todo.js
@@ -0,0 +1,14 @@
+// https://scotch.io/tutorials/routing-react-apps-the-complete-guide
+
+
+const requireAuth = (nextState, replace) => {
+    if (!auth.isAdmin()) {
+        // Redirect to Home page if not an Admin
+        replace({ pathname: '/' })
+    }
+}
+export const AdminRoutes = () => {
+  return (
+     <Route path="/admin" component={Admin} onEnter={requireAuth} />
+  )
+}
